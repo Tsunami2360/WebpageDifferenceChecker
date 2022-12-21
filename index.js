948bfd8210614497ab58f5fd9aabc177c0b28d37
Tsunami <39643347+Tsunami2360@users.noreply.github.com>
2022-12-21 21:51:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,27 +3,37 @@ const fs = require("fs");
 const robotjs = require("robotjs");
 
 let i = 0;
-setInterval(() => {
-  fetch("YOUR_WEBPAGE_URL")
+function preCheck() {
+  fetch("https://www.your-tickets.be/events/BoitetheFuck")
   .then(res => res.text())
   .then(data => {
     fs.writeFile("./data", data, (err) => {
-      if(err) {
-        console.log(err);
-      } else {
-        if(data === fs.readFileSync("./data", {encoding: "utf8"})) {
-          console.log(`${i}. ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()} same`);
-          i++;
-        } else {
-          //DO SOMETHING
-          /*console.log("DIFFERENT");
-          robotjs.moveMouse(793, 843);
-          robotjs.mouseClick();
-          robotjs.moveMouse(412, 770);
-          robotjs.typeString(":rotating_light: WAVE 2 STAAT ONLINE!!! :rotating_light: https://www.your-tickets.be/events/BoitetheFuck");
-          robotjs.keyTap("enter");*/
-        }
-      }
+      if(err) console.log(err);
     });
   });
-}, 15000);
+}
+preCheck();
+
+function check() {
+  setInterval(() => {
+    fetch("https://www.your-tickets.be/events/BoitetheFuck")
+    .then(res => res.text())
+    .then(data => {
+      if(data === fs.readFileSync("./data", {encoding: "utf8"})) {
+        console.log(`${i}. ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()} same`);
+        i++;
+        fs.writeFile("./data", data, (err) => {
+          if(err) console.log(err);
+        });
+      } else {
+        console.log("DIFFERENT");
+        robotjs.moveMouse(793, 843);
+        robotjs.mouseClick();
+        robotjs.moveMouse(412, 770);
+        robotjs.typeString(":rotating_light: WAVE 2 STAAT ONLINE!!! :rotating_light: https://www.your-tickets.be/events/BoitetheFuck");
+        robotjs.keyTap("enter");
+      }
+    });
+  }, 15000);
+}
+setTimeout(() => check(), 15000);
